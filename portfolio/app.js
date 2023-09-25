02094500c599da08d3f15160462db3e0621daa6c
soujnya <soujnyaacharya999@gmail.com>
2023-09-25 21:31:24 +0530

    new-commit

--- portfolio/app.js
+++ portfolio/app.js
@@ -3,10 +3,14 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
+
+
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
@@ -16,6 +20,9 @@ document.addEventListener('scroll', () => {
 		header.style.backgroundColor = 'transparent';
 	}
 });
+
+
+
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
